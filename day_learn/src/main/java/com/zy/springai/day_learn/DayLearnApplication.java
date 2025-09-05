aa6e3dd181b3603d3354e799c9ecbc5812b2d6a6
zy <zy@qq.com>
2025-09-05 16:20:16 +0800

    feat: 新增日常学习模块

---
+++ day_learn/src/main/java/com/zy/springai/day_learn/DayLearnApplication.java
@@ -0,0 +1,13 @@
+package com.zy.springai.day_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayLearnApplication.class, args);
+    }
+
+}
---
+++ day_learn/src/test/java/com/zy/springai/day_learn/DayLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.springai.day_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
